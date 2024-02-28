d2126e571604f5997c5cda1fb3f32e04b9ae3aba
Zed Barker-Flower <Zed.Barker-Flower@idhl.co.uk>
2024-02-28 10:47:02 +0000

    updated to use VS2022 & dotnet core

--- src/NetC.DeveloperExam.WebCore/App_Start/FilterConfig.cs
+++ src/NetC.DeveloperExam.WebCore/App_Start/FilterConfig.cs
@@ -1,7 +1,6 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace NetC.JuniorDeveloperExam.Web
+namespace NetC.DeveloperExam.WebCore
 {
     public class FilterConfig
     {
